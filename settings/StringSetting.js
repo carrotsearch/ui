15bf967315d0c5f946ad316552af2844fd77e40b
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2021-02-04 17:47:56 +0100

    Fix side panel scrolling, multi line string setting.

--- settings/StringSetting.js
+++ settings/StringSetting.js
@@ -2,18 +2,32 @@ import React from "react";
 
 import { view } from "@risingstack/react-easy-state";
 
-import { InputGroup } from "@blueprintjs/core";
+import { InputGroup, TextArea } from "@blueprintjs/core";
 import { Setting } from "./Setting.js";
 
-export const StringSetting = view(({ setting, get, set }) => {
-  const { label, description } = setting;
+export const StringSetting = view(
+  ({ setting, get, set }) => {
+    const { label, description, multiLine = false } = setting;
 
-  return (
-    <Setting className="StringSetting" label={label} description={description}>
-      <InputGroup
-        value={get(setting)}
-        onChange={e => set(setting, e.target.value)}
-      />
-    </Setting>
-  );
-});
+    return (
+      <Setting
+        className="StringSetting"
+        label={label}
+        description={description}
+      >
+        {multiLine ? (
+          <TextArea
+            style={{ width: "100%", minHeight: "8rem" }}
+            value={get(setting)}
+            onChange={e => set(setting, e.target.value)}
+          />
+        ) : (
+          <InputGroup
+            value={get(setting)}
+            onChange={e => set(setting, e.target.value)}
+          />
+        )}
+      </Setting>
+    );
+  }
+);
