db95c24dfa95366e9bf7398394b5410337a34bec
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2021-10-18 11:54:33 +0200

    More comparators.

--- lang/comparator.js
+++ lang/comparator.js
@@ -8,8 +8,27 @@ export const naturalComparator = (a, b) => {
   }
 };
 
+export const comparatorOn = (fn, comparator) => {
+  return (a, b) => {
+    return comparator(fn(a), fn(b));
+  };
+}
+
 export const naturalComparatorOn = prop => {
   return (a, b) => naturalComparator(a[prop], b[prop]);
+};
+
+export const chainedComparator = (...comparators) => {
+  return (a, b) => {
+    for (let c of comparators) {
+      const comp = c(a, b);
+      if (comp !== 0) {
+        return comp;
+      }
+    }
+
+    return 0;
+  }
 };
 
 export const reversedComparator = comparator => {
