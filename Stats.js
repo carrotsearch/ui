6dc3444d623a88a5dd1a0fcdfb2b719b6dfaf44f
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2021-10-27 09:27:39 +0200

    Stats enhancements.

--- Stats.js
+++ Stats.js
@@ -3,10 +3,20 @@ import React from "react";
 import "./Stats.css";
 
 import classnames from "classnames";
+import { humanizeCount, pluralize } from "./lang/humanize.js";
 
-export const Stat = ({ value, label }) => {
+export const countStat = ({ count, what, id = "", divider = false }) => {
+  return {
+    id: `${what}:count:${id}`,
+    value: humanizeCount(count),
+    label: pluralize(count, what, false),
+    divider: divider
+  };
+};
+
+export const Stat = ({ value, label, className, divider = false }) => {
   return (
-    <div className="Stat">
+    <div className={classnames("Stat", className, { WithDivider: divider })}>
       <strong>{value}</strong>
       <small>{label}</small>
     </div>
