8d250d0ba6f746a77d2b97897fe1da1d95f58dd8
Stanislaw Osinski <stanislaw.osinski@carrotsearch.com>
2024-01-29 14:03:34 +0100

    Fix initialization of persistent store.

--- store/persistent-store.js
+++ store/persistent-store.js
@@ -3,19 +3,19 @@ import storage from "store2";
 import LRU from "lru-cache";
 
 export const persistentStore = (key, defaults, methods) => {
+  const resolvedDefaults = defaults instanceof Function ? defaults() : defaults;
+
   const backingStore = store(
-    Object.assign({}, defaults, storage.get(key), methods)
+    Object.assign({}, resolvedDefaults, storage.get(key), methods)
   );
 
   autoEffect(function () {
     storage.set(key, backingStore);
   });
 
-  const resolvedDefaults = defaults instanceof Function ? defaults() : defaults;
-
   backingStore.resetToDefaults = () => {
     // Assume a flat object for now
-    Object.keys(defaults).forEach(prop => {
+    Object.keys(resolvedDefaults).forEach(prop => {
       backingStore[prop] = resolvedDefaults[prop];
     });
   };
